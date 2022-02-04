7ec02cb2ef5040ef2dfc76221a2f2841f13083b9
TrongNghia <htnghia101997@gmail.com>
2022-02-04 13:47:11 +0700

    add Dice app

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.happybirthday
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -17,23 +18,56 @@ class MainActivity : AppCompatActivity() {
         val btnButton: Button = findViewById(R.id.button)
         btnButton.setOnClickListener {
 
-            val firstDice: Dice = Dice(10)
-            val result: Int = firstDice.roll()
+            onRoll ()
+        }
+
+    }
+
+    fun onRoll (){
+
+        val firstDice: Dice = Dice(6)
+        val result: Int = firstDice.roll()
 
             val tvNumber: TextView = findViewById(R.id.number)
             tvNumber.text = result.toString()
+        val imgNumber: ImageView = findViewById(R.id.imgDice)
+//        imgNumber.setImageResource(R.drawable.dice_6)
+        Toast.makeText(this, result.toString(), Toast.LENGTH_SHORT).show()
 
+        var drawable: Int = 0
+        var string: String = ""
+
+        when (result) {
+            1 -> {
+                drawable = R.drawable.dice_1
+                string = "you win"
+            }
+            2 -> {
+                drawable = R.drawable.dice_2
+                string = "you lose"
+            }
+            3 -> {
+                drawable = R.drawable.dice_3
+                string = "you lose"
+            }
+            4 -> {
+                drawable = R.drawable.dice_4
+                string = "you lose"
+            }
+            5 -> {
+                drawable = R.drawable.dice_5
+                string = "you lose"
+            }
+            6 -> {
+                drawable = R.drawable.dice_6
+                string = "you lose"
+            }
         }
 
-
-
-
-
-//        firstDice.sides = 20
-//        val resultChange: Int = firstDice.roll()
-//        Log.i("MainActivity","Random: ${resultChange}")
-
+        imgNumber.setImageResource(drawable)
+        tvNumber.text = string
 
     }
+
 
 }
